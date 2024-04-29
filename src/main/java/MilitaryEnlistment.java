de3ae5a012e146046a576f3660ca27d5c1a4f390
Gustavo Pontes <devgustavopontes@gmail.com>
2024-04-29 14:45:33 -0300

    Adicionando arquivo MilitaryEnlistment

---
+++ src/main/java/MilitaryEnlistment.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MilitaryEnlistment {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o sexo [M/F]: ");
+        String sex = input.next();
+        System.out.print("Digite a idade: ");
+        int age = input.nextInt();
+
+        if (sex.equalsIgnoreCase("M") && age >= 18) {
+            System.out.println("Alistamento obrigatório");
+        } else if (sex.equalsIgnoreCase("F") && age >= 18) {
+            System.out.print("Você deseja se alistar? [S/N] ");
+            String response = input.next();
+            if (response.equalsIgnoreCase("S")) {
+                System.out.println("Alistamento concluído com sucesso");
+            } else {
+                System.out.println("Não alistada");
+            }
+        } else {
+            System.out.println("Alistamento não permitido");
+        }
+    }
+}
